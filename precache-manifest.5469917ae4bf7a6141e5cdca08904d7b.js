73d697aa989b21cf907ac0f7e53b4c9b3408cf09
saadatt <saadatt@gmail.com>
2020-02-24 16:49:26 -0600

    Updates

---
+++ precache-manifest.5469917ae4bf7a6141e5cdca08904d7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8c4be69ffa66af7a743c257589c5d6",
+    "url": "/cra3/index.html"
+  },
+  {
+    "revision": "bb1302572cd1e2015495",
+    "url": "/cra3/static/css/main.1378db2f.chunk.css"
+  },
+  {
+    "revision": "df07a820c53066f92beb",
+    "url": "/cra3/static/js/2.8646f2df.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra3/static/js/2.8646f2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1302572cd1e2015495",
+    "url": "/cra3/static/js/main.3a738304.chunk.js"
+  },
+  {
+    "revision": "614eb1865c70639525c4",
+    "url": "/cra3/static/js/runtime-main.5af43166.js"
+  },
+  {
+    "revision": "8bb11f2ed5398d00b298e53cd0d99508",
+    "url": "/cra3/static/media/copyright2.8bb11f2e.jpg"
+  },
+  {
+    "revision": "d217ae638c066e003ea0d08f268162c2",
+    "url": "/cra3/static/media/supremelogo.d217ae63.png"
+  }
+]);
